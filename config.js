d7915b7ed415fcfc8c8381bbdb934de004d2cd66
lxyisooo <thesecondloyi@gmail.com>
2026-04-15 17:18:07 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  prefix: "mb",
+
+  admins: ["1451533934130364467"],
+
+  economy: {
+    startCash: 2500
+  },
+
+  blackjack: {
+    dealerStand: 17
+  },
+
+  pets: {
+    fusionMultiplier: 1.8
+  },
+
+  raids: {
+    cooldown: 3600000
+  }
+};
