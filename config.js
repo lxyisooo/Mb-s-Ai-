ddfaef0d17e06915c80af93b75a5a3c6d2aecc61
lxyisooo <thesecondloyi@gmail.com>
2026-04-15 17:27:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,32 @@
 module.exports = {
   prefix: "mb",
-
   admins: ["1451533934130364467"],
 
   economy: {
-    startCash: 2500
+    startCash: 2500,
+    daily: 6000
   },
 
-  blackjack: {
-    dealerStand: 17
+  casino: {
+    slotsMulti: 5,
+    blackjackMulti: 2,
+    jackpotChance: 0.03
   },
 
   pets: {
-    fusionMultiplier: 1.8
+    list: {
+      cat: { cost: 10000, power: 1, multi: 0.1 },
+      wolf: { cost: 50000, power: 3, multi: 0.25 },
+      dragon: { cost: 250000, power: 8, multi: 0.6 }
+    }
   },
 
   raids: {
-    cooldown: 3600000
+    cooldown: 3600000,
+    baseBank: 100000
+  },
+
+  seasons: {
+    duration: 7 * 24 * 60 * 60 * 1000
   }
 };
