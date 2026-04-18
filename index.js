9016bd9d458b6c052e6f673e9345c2be203af175
lxyisooo <thesecondloyi@gmail.com>
2026-04-18 19:48:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -99,9 +99,11 @@ client.on('interactionCreate', async (interaction) => {
         .addOptions(
           { label: 'Tuffest People in Chat', value: '1474660849447866410', emoji: ':001criipyimcool:' },
           { label: 'Doritos', value: '1477694222307168326', emoji: ':nerd~1:' },
-          { label: 'Morvani Automative LLC', value: '1477700900138254608', emoji: { id '<1472222233215893555>' }
-        )
-    );
+          {
+  label: 'Morvani Automotive LLC',
+  value: '1477700900138254608',
+  emoji: { id: '1472222233215893555' }
+          }
 
     const menu4 = new ActionRowBuilder().addComponents(
       new StringSelectMenuBuilder()
